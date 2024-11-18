cc9f46e0d95717f5b9bb3f9b8698bd9b18feceb3
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-11-18 09:52:08 +0100

    efed

---
+++ Övningar 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// string [] names = ["Monster truck", "Pokemon", "Bilar", "Bolibompa draken", "Banan"];
+
+// for (int i = 0; i < names.Length; i++)
+// {
+//     Console.WriteLine(names[i]);
+// }
+
+// Console.ReadLine();
+
+// string [] names = ["Dean", "Martin", "Joakim", "Anton", "Theodor"];
+
+// int [] nummer = {1, 2, 5, 8, 7};
+
