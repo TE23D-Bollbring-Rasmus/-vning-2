954adfa235ef965f9196d5958b49693556fa2477
Nils Rasmus Mattsson Bollbring <rasmus.mattssonbollbring@elev.ga.ntig.se>
2024-11-22 11:18:46 +0100

    uiul

--- Övningar 2/Program.cs
+++ Övningar 2/Program.cs
@@ -1,13 +1,15 @@
-﻿// string [] names = ["Monster truck", "Pokemon", "Bilar", "Bolibompa draken", "Banan"];
+﻿// string [] namess = ["Monster truck", "Pokemon", "Bilar", "Bolibompa draken", "Banan"];
 
-// for (int i = 0; i < names.Length; i++)
-// {
-//     Console.WriteLine(names[i]);
-// }
-
-// Console.ReadLine();
 
 // string [] names = ["Dean", "Martin", "Joakim", "Anton", "Theodor"];
 
 // int [] nummer = {1, 2, 5, 8, 7};
 
+// for (int i = 0; i < namess.Length; i++)
+// {
+//     Console.WriteLine($"{names[i]} ger {namess[i]} betyget {nummer[i]}");
+// }
+
+// Console.ReadLine();
+
+string [] cities = [];
